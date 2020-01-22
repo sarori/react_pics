c27335d19cb4ca07b26bc543b9a7b14fa040409d
sarori <salo3678@gmail.com>
2020-01-22 17:45:36 +0900

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get("https://api.unsplash.com/search/photos", {
+			params: { query: term },
+			headers: {
+				Authorization:
+					"Client-ID 19af61cb400939e462bc46f32e3db606f70657fe087fa1450a56179c1736f9a1"
+			}
+		});
 	}
 	render() {
 		return (
